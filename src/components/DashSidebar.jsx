b3cdf7556ea252837eb6fb0de9abc641215dc66c
Visheshyadav8652 <162328787+Visheshyadav8652@users.noreply.github.com>
2025-01-13 22:58:24 +0530

    remove exe

---
+++ src/components/DashSidebar.jsx
@@ -0,0 +1,22 @@
+import { Sidebar } from 'flowbite-react'
+import {HiUser,HiArrowSmRight} from 'react-icons/hi';
+import React from 'react'
+
+const DashSidebar = () => {
+  return (
+    <Sidebar>
+        <Sidebar.Items>
+            <Sidebar.ItemGroup>
+                <Sidebar.Item active icon={HiUser} label={"User"} labelColor="dark">
+                    Profile
+                </Sidebar.Item>
+                <Sidebar.Item active icon={HiArrowSmRight} className="cursor-pointer" >
+                    Sign Out
+                </Sidebar.Item>
+            </Sidebar.ItemGroup>
+        </Sidebar.Items>
+    </Sidebar>
+  )
+}
+
+export default DashSidebar;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import { useState,useEffect } from "react"
+
+import { useLocation } from "react-router-dom"
+import DashProfile from "../components/DashProfile";
+import DashSidebar from "../components/DashSidebar";
 
 const Dashboard = () => {
+  const location = useLocation();
+  const [tab,setTab]=useState('');
+  useEffect(()=>{
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl=urlParams.get('tab');
+    if (tabFromUrl){
+      setTab(tabFromUrl);
+    }
+  },[location.search]);
   return (
-    <div>Dashboard 2 to see the changes in the github</div>
+    <div>
+      <div className="">
+        {/* Sidebar */}
+        <DashSidebar/>
+      </div>
+        {/* profile */}
+        {tab==='profile'&&<DashProfile/>}
+    </div>
   )
 }
 
